be21c718c1f884a3e19bd36b8631a94f8a722f1d
cesarNeZe <cesarzaitoun@gmail.com>
2024-08-05 11:23:31 +0300

    report vitals

---
+++ reportWebVitals.d.ts
@@ -0,0 +1,8 @@
+// src/reportWebVitals.d.ts
+
+declare module './reportWebVitals' {
+    const reportWebVitals: (onPerfEntry?: (metric: any) => void) => void;
+    export default reportWebVitals;
+  }
+
+  
